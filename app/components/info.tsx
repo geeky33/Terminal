5131777f169b0867b75fcbb13130d198692025d1
harshal-ishere <harshal011kamble@gmail.com>
2024-10-12 19:01:39 +0530

    changed the starting input symbol changed it in all the files added logic to focus on the input tag to focus on the input element whenever the page loads

--- app/components/info.tsx
+++ app/components/info.tsx
@@ -1,20 +1,20 @@
 import { FC } from "react";
 interface incProps {
-    title: string,
-    p1: string,
-    p2: string,
-    p3: string
+  title: string,
+  p1: string,
+  p2: string,
+  p3: string
 }
 let Info: FC<incProps> = ({ title, p1, p2, p3 }) => {
-    return (
-        <div className="flex flex-col gap-2">
-            <p className="dollar" >$<span className=" text-white ">  {title}</span></p>
-            <div className="flex flex-col gap-1">
-                <p>{p1}</p>
-                <p>{p2}</p>
-                <p>{p3}</p>
-            </div>
-        </div>
-    )
+  return (
+    <div className="flex flex-col gap-2">
+      <p className="dollar" >{'>'}<span className=" text-white ">  {title}</span></p>
+      <div className="flex flex-col gap-1">
+        <p>{p1}</p>
+        <p>{p2}</p>
+        <p>{p3}</p>
+      </div>
+    </div>
+  )
 }
 export default Info;
--- app/components/start.tsx
+++ app/components/start.tsx
@@ -1,26 +1,26 @@
-let Start=()=>{
-    return (
-        <>
-        <p className="dollar" >$<span className=" text-white ">  start</span></p>
-            <div className="Start flex flex-row gap-24">
-                <div className="flex flex-col">
-                    <p>about</p>
-                    <p>edu</p>
-                    <p>skills</p>
-                    <p>projects</p>
-                    <p>contacts</p>
-                    <p>clear</p>
-                </div>
-                <div className="flex flex-col">
-                    <p>About me</p>
-                    <p>My Education</p>
-                    <p>My Skills</p>
-                    <p>My Projects</p>
-                    <p>Contact Me</p>
-                    <p>Clear the terminal</p>
-                </div>
-            </div>
-        </>
-    )
+let Start = () => {
+  return (
+    <>
+      <p className="dollar" >{'>'}<span className=" text-white ">  start</span></p>
+      <div className="Start flex flex-row gap-24">
+        <div className="flex flex-col">
+          <p>about</p>
+          <p>edu</p>
+          <p>skills</p>
+          <p>projects</p>
+          <p>contacts</p>
+          <p>clear</p>
+        </div>
+        <div className="flex flex-col">
+          <p>About me</p>
+          <p>My Education</p>
+          <p>My Skills</p>
+          <p>My Projects</p>
+          <p>Contact Me</p>
+          <p>Clear the terminal</p>
+        </div>
+      </div>
+    </>
+  )
 }
 export default Start;
